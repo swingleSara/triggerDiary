f8350d12688fd8432506927e8070480a2b146c43
Sara Swingle <sara.swingle.dev@gmail.com>
2022-08-12 14:48:42 -0600

    Initial commit, working app

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+
+//Description: Auth with Google
+//Route: GET /auth/google
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+//Description: Google auth callback
+//Route: GET /auth/google/callback
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/dashboard");
+  }
+);
+
+//Description: Logout user
+//Route: /auth/logout
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
