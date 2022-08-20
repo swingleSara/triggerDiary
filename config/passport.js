d7e62161ca0d9562ea6867a004a8aa78e427a205
Sara Swingle <sara.swingle.dev@gmail.com>
2022-08-19 19:43:40 -0600

    Update passport.js documentation

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const User = require("../models/User");
+const dotenv = require("dotenv");
 
 module.exports = function (passport) {
   passport.use(
