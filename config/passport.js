ea2f18978cba8112280b18edf14aeda8350a05f0
Sara Swingle <sara.swingle.dev@gmail.com>
2022-08-21 10:35:47 -0600

    Update passport.js documentation

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const User = require("../models/User");
-const dotenv = require("dotenv");
 
 module.exports = function (passport) {
   passport.use(
@@ -9,7 +8,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "auth/google/callback",
+        callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
